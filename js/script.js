914d569bd30d999f8dd12f5861bb29aac718d604
agustinZavalaAguilar <88766456+agustinZavalaAguilar@users.noreply.github.com>
2024-01-09 16:45:32 +0100

    animation step1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 console.log('hello');
 
 function start () {
-    console.log('test');
-}
+    
+    var step1 = document.getElementById('step1');
+    var title = document.getElementById('voeux');
+    var imgPhilosophe = document.getElementById('imgPhilosophe');
+    var citation = document.getElementById('citation');
+
+    step1.style.opacity = 1;
+    title.style.opacity = 1;
+    setTimeout(() => {
+        title.style.top = 0;
+    }, "1000");
+    setTimeout(()=> {
+        imgPhilosophe.style.opacity = 1;
+    },"2000");
+    citation.style.opacity=1;      
+    citation.style.bottom = 0;
+    }
 
