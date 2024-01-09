1721629d2a8fc8293bf2fd262fb8f5c7320fc400
agustinZavalaAguilar <88766456+agustinZavalaAguilar@users.noreply.github.com>
2024-01-09 17:00:41 +0100

    sortie step1

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 console.log('hello');
 
 function start () {
-    
+    //declaration de variables
     var step1 = document.getElementById('step1');
     var title = document.getElementById('voeux');
     var imgPhilosophe = document.getElementById('imgPhilosophe');
     var citation = document.getElementById('citation');
-
+    //entrée
     step1.style.opacity = 1;
     title.style.opacity = 1;
     setTimeout(() => {
@@ -17,5 +17,12 @@ function start () {
     },"2000");
     citation.style.opacity=1;      
     citation.style.bottom = 0;
+    //sortie
+    setTimeout(() => {
+        step1.style.opacity = 0;  
+        title.style.opacity = 0;
+        imgPhilosophe.style.opacity = 0;
+        citation.style.opacity=0; 
+    }, '8000');
     }
 
